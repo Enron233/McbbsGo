9ed1fd84dd22f73742d8445678f4d701d6716134
Enron <286526681@qq.com>
2022-02-05 17:01:07 +0800

    First

---
+++ GetServerList.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+myHeader = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+}
+
+
+def GetServerUrl(pageNum):
+    url = "http://www.mcbbs.net/forum-server-" + str(pageNum) + ".html"
+    resp = requests.get(url, headers=myHeader)
+    contain = BeautifulSoup(resp.text, "html.parser")
+    firstFind = contain.find("table", attrs={"summary": "forum_179"})
+    secondFind = firstFind.find_all("a", attrs={"onclick": "atarget(this)"})
+    resp.close()
+    return secondFind
